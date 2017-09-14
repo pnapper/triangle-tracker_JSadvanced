a8a4bc2e7a25ea51e21b94f9fc0ac3844010ce95
Daniel Cheng and Gyles Batara <daniel@>
2017-09-14 14:23:18 -0700

    Add HTML and part of js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  $("#formOne").submit(funtion(event) {
+    var side1 = $("input#side1").val();
+    var side2 = $("input#side2").val();
+    var side3 = $("input#side3").val();
+  }
+});
