4a632c95e79b632c777c93b0727cdb4e0e65e51d
Daniel Cheng and Gyles Batara <daniel@>
2017-09-14 15:11:44 -0700

    finish js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
 
-  $("#formOne").submit(funtion(event) {
-    var side1 = $("input#side1").val();
-    var side2 = $("input#side2").val();
-    var side3 = $("input#side3").val();
-  }
+  $("#formOne").submit(function(event) {
+    $("#equilateral, #isosceles, #scalene, #not").hide();
+    var side1 = parseFloat($("input#side1").val());
+    var side2 = parseFloat($("input#side2").val());
+    var side3 = parseFloat($("input#side3").val());
+    if (side1 > 0 && side2 > 0 && side3 > 0) {
+      if (side1 === side2 || side2 === side3 || side3 === side1) {
+        if (side1 === side2 && side2 === side3) {
+          $("#equilateral").show();
+        } else if (side1 !== side2 || side2 !== side3 || side3 !== side1) {
+          $("#isosceles").show();
+        }
+      } else if (side1 !== side2 && side2 !== side3 && side3 !== side1) {
+          $("#scalene").show();
+      } else {
+        $("#not").show();
+      }
+    } else {
+      $("#not").show();
+    }
+    event.preventDefault();
+  });
 });
